5e3f0419ce80bd17711ee770745f6439dbbebcd2
Errol W <ewatsonrei@gmail.com>
2019-01-11 18:26:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 var recipes = new Object({});
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return `${key}: ${value}`;
+var obj = {
+  key: value
+};
+
+  return obj;
+// returns original key value pair, and a new key value pair
 }
+
+/*
+
+describe('updateObjectWithKeyAndValue(object, key, value)', function() {
+    it('returns an object with the orignal key value pairs and the new key value pair', function() {
+      var obj = { prop: 1 }
+
+      expect(updateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
+        prop: 1,
+        prop2: 2
+      })
+    })
+
+*/
+
+
+
+
+
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   
 }
 
 function deleteFromObjectByKey(object, key) {
-  
+
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  
+
 }
